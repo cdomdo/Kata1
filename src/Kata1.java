760b7a42fa5e52355ca55c74741a0049698991ef
Carlos <dominguezdopazocarlos@gmail.com>
2023-09-25 16:59:14 +0100

    Kata1 class finished.

--- src/Kata1.java
+++ src/Kata1.java
@@ -1,2 +1,9 @@
+import java.time.LocalDate;
+
 public class Kata1 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2002, 6, 17);
+        Person person = new Person("Carlos", date);
+        System.out.println(String.format("%s tiene %d años.", person.getName(), person.getAge()));
+    }
 }
