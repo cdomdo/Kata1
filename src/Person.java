52d0e2832fca94c6d331a0bd321a524c6458df54
Carlos <dominguezdopazocarlos@gmail.com>
2023-09-25 16:54:45 +0100

    Person class finished.

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,24 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
+    private String name;
+    private LocalDate birthdate;
+
+    public Person(String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public int getAge() {
+        return (int) Period.between(birthdate, LocalDate.now()).getYears();
+    }
 }
